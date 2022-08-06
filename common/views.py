2e971a9358df2ff77ee63a38d82494a6142e9422
daesung-jin <citijds@naver.com>
2022-08-06 13:35:53 +0900

    404

--- common/views.py
+++ common/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def page_not_found(request, exception):
+    """
+    404 Page not found
+    """
+    return render(request, 'common/404.html', {})
