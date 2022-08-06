f968fc6a4360265e6e92d9a939724d41063ca423
daesung-jin <citijds@naver.com>
2022-08-06 14:54:11 +0900

    500에러

--- common/views.py
+++ common/views.py
@@ -7,3 +7,10 @@ def page_not_found(request, exception):
     404 Page not found
     """
     return render(request, 'common/404.html', {})
+
+
+def page_not_found(request, exception):
+    """
+    500 Page not found
+    """
+    return render(request, 'common/500.html', {})
